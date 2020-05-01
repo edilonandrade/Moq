7937af016e5163ede11ce90994b4b7b1858d5ebb
Edilon Andrade <edilonandrade@gmail.com>
2020-05-01 17:48:02 -0300

    Adicionando teste para CadastraTarefa.

--- Alura/testes-integracao/tests/Alura.CoisasAFazer.Testes/UnitTest1.cs
+++ Alura/testes-integracao/tests/Alura.CoisasAFazer.Testes/UnitTest1.cs
@@ -1,3 +1,6 @@
+using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Services.Handlers;
 using System;
 using Xunit;
 
@@ -8,7 +11,15 @@ namespace Alura.CoisasAFazer.Testes
         [Fact]
         public void Test1()
         {
+            //Arrange
+            var comando = new CadastraTarefa("Estudar XUnit", new Categoria("Estudo"), new DateTime(2020, 12, 31));
+            var handler = new CadastraTarefaHandler();
 
+            //Act
+            handler.Execute(comando);
+
+            //Assert
+            Assert.True(true);
         }
     }
 }
