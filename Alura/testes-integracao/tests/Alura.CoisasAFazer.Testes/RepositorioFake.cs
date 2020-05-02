ea11f7e3eae390b94fbe456b1968b5b6013450f6
Edilon Andrade <edilonandrade@gmail.com>
2020-05-02 01:10:48 -0300

    Adicionando repositório fake.

---
+++ Alura/testes-integracao/tests/Alura.CoisasAFazer.Testes/RepositorioFake.cs
@@ -0,0 +1,36 @@
+﻿using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    class RepositorioFake : IRepositorioTarefas
+    {
+        public void AtualizarTarefas(params Tarefa[] tarefas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirTarefas(params Tarefa[] tarefas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncluirTarefas(params Tarefa[] tarefas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria ObtemCategoriaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tarefa> ObtemTarefas(Func<Tarefa, bool> filtro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
