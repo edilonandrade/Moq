830bcbd13790cdb38b9af9c252e6c8a90ace9275
Edilon Andrade <edilonandrade@gmail.com>
2020-05-18 08:27:23 -0300

    Teste na controller quando ocorrer exceção (500).

--- Alura/testes-integracao/tests/Alura.CoisasAFazer.Testes/TarefasControllerEndpointCadastraTarefa.cs
+++ Alura/testes-integracao/tests/Alura.CoisasAFazer.Testes/TarefasControllerEndpointCadastraTarefa.cs
@@ -44,5 +44,32 @@ namespace Alura.CoisasAFazer.Testes
             //assert
             Assert.IsType<OkResult>(retorno);
         }
+
+        [Fact]
+        public void QuandoExcecaoForLancadaDeveRetornarStatusCode500()
+        {
+            //arrange
+            var MockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            var mockRepo = new Mock<IRepositorioTarefas>();
+            mockRepo.Setup(r => r.ObtemCategoriaPorId(20)).Returns(new Categoria(20, "Estudo"));
+            mockRepo.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>())).Throws(new Exception("Houve um erro"));
+
+            var repo = mockRepo.Object;
+
+            var controlador = new TarefasController(repo, MockLogger.Object);
+            var model = new CadastraTarefaVM();
+            model.IdCategoria = 20;
+            model.Titulo = "Estudar  Xunit";
+            model.Prazo = new DateTime(2020, 12, 31);
+
+            //act
+            var retorno = controlador.EndpointCadastraTarefa(model);
+
+            //assert
+            Assert.IsType<StatusCodeResult>(retorno);
+            var statusCodeRetornado = (retorno as StatusCodeResult).StatusCode;
+            Assert.Equal(500, statusCodeRetornado);
+        }
     }
 }
